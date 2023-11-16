23c9b71ffe98e71d72ea42c953b8d30d00699d09
alxfu <43872350+alxfu@users.noreply.github.com>
2023-11-16 10:36:47 +0100

    main.py experimental file list and ffmpeg for linux
    updated README.md

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import subprocess
 from os import path
 import browser_cookie3
 import requests
 
 # URL - paste in "url" line. check quotes!
-url = "https://video.app-eur.cvent.com/pr53/d720b6a7-36d6-4e27-bbf4-841a6e32dc5f/cb378920-e6fd-4fa7-8a99-ac5767910dcb/c5c61198-4e41-4417-8021-4b4fb70c55ee/converted-videos-1699895593/hls/master_1080_1920_0_3500kbps_00111.ts?Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6Imh0dHBzOi8vdmlkZW8uYXBwLWV1ci5jdmVudC5jb20vcHI1My9kNzIwYjZhNy0zNmQ2LTRlMjctYmJmNC04NDFhNmUzMmRjNWYvY2IzNzg5MjAtZTZmZC00ZmE3LThhOTktYWM1NzY3OTEwZGNiL2M1YzYxMTk4LTRlNDEtNDQxNy04MDIxLTRiNGZiNzBjNTVlZS9jb252ZXJ0ZWQtdmlkZW9zLTE2OTk4OTU1OTMvaGxzLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3MDAwNjI4NzB9fX1dfQ__&Signature=DFlIrSpgFH7kfJJqZSDNPQXZSfp317dkCCD22E0xHYScTjbO65Xg3P4ymXaLxNSasfqmp84YbuAhapWOt6rGrr-SPzspUBfdxNKtVs0hM7P6h~8-VYUPKylHI4RUBDmx7ieLH7PGG024VQgiyxrhDjOjLi6ggBxQLggN2tz-nuXC-urymF5Hhwke-jdFpqcomxd1WVcjqSHdy4NyEonHptoqbZ3Xj6liw2edi2axzuOnzXpkn34MgJunzip9noLmBQl~qpCudSWZDo0w9nhUFWx0jx0uXL8N20g3k0nLZNHUuEg-d25nCwLNqlH7sQyaqGpe~ypCsdTUTWeZPDqFmQ__&Key-Pair-Id=K2TLR9K095SRQ3"
+url = "https://video.app-eur.cvent.com/pr53/d720b6a7-36d6-4e27-bbf4-841a6e32dc5f/cb378920-e6fd-4fa7-8a99-ac5767910dcb/a0c805e0-dd59-4231-937b-02fc9e6858d1/converted-videos-1696523735/hls/video_1080_1920_0_3500kbps_00011.ts?Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6Imh0dHBzOi8vdmlkZW8uYXBwLWV1ci5jdmVudC5jb20vcHI1My9kNzIwYjZhNy0zNmQ2LTRlMjctYmJmNC04NDFhNmUzMmRjNWYvY2IzNzg5MjAtZTZmZC00ZmE3LThhOTktYWM1NzY3OTEwZGNiL2EwYzgwNWUwLWRkNTktNDIzMS05MzdiLTAyZmM5ZTY4NThkMS9jb252ZXJ0ZWQtdmlkZW9zLTE2OTY1MjM3MzUvaGxzLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3MDAxNDc2ODF9fX1dfQ__&Signature=YPfflYdwK1sgKL9vF-iPUNDAVk0Ag5UVy3gW-4yXNz8dn90pLHgcjZm8KVFCbMYmn88AVwwBMiZ4JDmqV-kh93b4-hVplrdFzdrx~SQA62TFqcOfTPMIBHsDXr~iYObYNSZdE5N-QC5RUKvklVPlxI6BovKm0VTP9CbF6RlwV0g7nbFSWaaqoRzJBWjC69Sn6Z7fx7lYRut8lUP1RjP1ghQ11VOR9Z9visLMK2fydFASGpiD6Ow7FHcFZhQmmQLVZICa0tqSWNA~52r7QwudWkj3P9UVRJjiZgvdozXlVD9NIrVCaCQ5-pIt9CIArfhJw2TeNIdFRDt45LtfSn-DHw__&Key-Pair-Id=K2TLR9K095SRQ3"
 url_split = url.split(".ts")
 size = len(url_split[0])
 url_split[0] = url_split[0][:size - 5]
@@ -43,9 +44,9 @@ ROOM TO SPLIT URL
 links = []
 
 
-def get_filename(url):
+def get_filename(myurl: str) -> str:
     #  create a filename from a URL leaving out HTTP, PATH, ending arguments e.g. "?2134452134&ewr5345"
-    fragment_removed = url.split("#")[0]  # keep to left of first #
+    fragment_removed = myurl.split("#")[0]  # keep to left of first #
     query_string_removed = fragment_removed.split("?")[0]
     scheme_removed = query_string_removed.split("://")[-1].split(":")[-1]
     if scheme_removed.find("/") == -1:
@@ -70,6 +71,46 @@ for url in links:
         print("NOT OKAY")
         break
 
+#  EXPERIMENTAL:
+#  create a list of the *.ts files
+
+#  WINDOWS - not working
+#  subprocess.run(['(for %i in (*.ts) do @echo file '%i') > mylist.txt'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+#  subprocess.check_output("(for %i in (*.ts) do @echo file '%i') > mylist.txt", shell=True, text=True)
+
+#  Linux
+subprocess.check_output("printf "file '%s'\n" *.ts > mylist.txt", shell=True, text=True)
+subprocess.check_output("ffmpeg -safe 0 -f concat -i mylist.txt -c copy output.mp4", shell=True, text=True)
 
 
 
+
+
+#  ffmpeg
+#  subprocess.run(['ffmpeg -safe 0 -f concat -i mylist.txt -c copy output.mp4'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+
+
+"""
+Automatically generating the input file
+
+It is possible to generate this list file with a bash for loop, or using printf. Either of the following would generate a list file containing every *.wav in the working directory:
+
+# with a bash for loop
+for f in *.wav; do echo "file '$f'" >> mylist.txt; done
+# or with printf
+printf "file '%s'\n" *.wav > mylist.txt
+
+On Windows Command-line:
+
+(for %i in (*.wav) do @echo file '%i') > mylist.txt
+
+Or for Windows Powershell:
+
+foreach ($i in Get-ChildItem .\*.wav) {"file '$i'" | Out-File mylist.txt -Encoding utf8 -Append}
+
+Or for Windows bat-file:
+
+(for %%i in (*.wav) do @echo file '%%i') > mylist.txt
+
+"""
